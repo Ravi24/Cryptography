4f1b8f36091032b313171e10405b5f7fae0d4c70
Ravi Bhushan <iqravi.bhushan@gmail.com>
2019-07-30 18:53:30 +0530

    Add files via upload

---
+++ CryptographyExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CryptographyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x= EncryptionExample();
+            
+            Console.WriteLine("hashed password ={0}", x);
+            Console.ReadLine();
+        }
+        public static string EncryptionExample()
+        {
+            Console.WriteLine("Enter a password: ");
+            string password = Console.ReadLine();
+            // generate a 128 bit sal using PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            StringBuilder xb = new StringBuilder();
+            for(int x= 0; x< salt.Length; x++)
+            {
+                xb.Append(salt[x].ToString("x2"));
+            }
+            Console.WriteLine("Salt = {0}", xb.ToString());
+            using (SHA512 sha512hash = SHA512.Create())
+            {
+                byte[] bytes = sha512hash.ComputeHash(Encoding.UTF8.GetBytes(password+xb.ToString()));
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    sb.Append(bytes[i].ToString());
+                }
+                return sb.ToString();
+            }
+            
+        }
+    }
+}
